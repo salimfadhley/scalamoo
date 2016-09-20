8eb1762434d7c3606fd09b8760dc196702559722
salim <slpg500a>
2016-09-20 20:28:32 +0100

    Major performance enhancment - now loading Pokedex stuff lazily

--- src/main/scala/model/pokedex/DexObject.scala
+++ src/main/scala/model/pokedex/DexObject.scala
@@ -1,14 +1,9 @@
 package model.pokedex
 
-/**
-  * Created by salim on 13/09/2016.
-  */
-
 trait DexClass {
   def uid: Int
 }
 
 trait DexObject[T <: DexClass] {
   def fromMap(row: Map[String, ConvertibleThing]): T
-
 }
